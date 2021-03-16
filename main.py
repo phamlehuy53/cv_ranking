1d605887b92cb67d3818e304a0b15451403b8660
hoang98long <53008046+hoang98long@users.noreply.github.com>
2021-03-16 16:13:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,52 +5,60 @@ from utils.ranking import *
 NUMBER_OF_CRITERIA = 12
 
 FEATURE_SCORING = {
-    'info':{    
+    'info': {
         'feature': ['CanEmail', 'CanTelNum', 'CanFullName'],
         'fn': None
     },
-    'introduction':{
-        'feature': ['CanDob', 'SexCode', 'CanAddress', 'CanNationality', 'Interest', 'Strength', 'FutureGoals' ],
-        'fn': None
-    }, 
-    'experience':{
-        'features': ['ExperienceYears'], 
+    'introduction': {
+        'feature': ['CanDob', 'SexCode', 'CanAddress', 'CanNationality', 'Interest', 'Strength', 'FutureGoals'],
         'fn': None
     },
-    'softskill':{
-
-    }
-
-    
+    'experience': {
+        'features': ['ExperienceYears'],
+        'fn': None
+    },
+    'softskill': {
+        'features': ['SoftSkill'],
+        'fn': None
+    },
+    'skill': {
+        'features': ['Skill'],
+        'fn': None
+    },
+    'graduated': {
+        'features': ['LearningDiploma', 'Specialize'],
+        'fn': None
+    },
+    'is_updated': {
+        'features': ['IsUpdate'],
+        'fn': None
+    },
+    'learning_awards': {
+        'features': ['LearningAwards'],
+        'fn': None
+    },
+    'working_awards': {
+        'features': ['WorkingAwards'],
+        'fn': None
+    },
+    'diploma': {
+        'features': ['Diploma'],
+        'fn': None
+    },
+    'cv_image': {
+        'features': ['CanImgCand'],
+        'fn': None
+    },
+    'language': {
+        'features': ['LanguageCertificate'],
+        'fn': None
+    },
+    'interest': {
+        'features': ['Interest'],
+        'fn': None
+    },
+    'reference_person': {
+        'features': ['ReferencePerson', 'ReferencePersonPosition'],
+        'fn': None
+    },
 }
-
-def pointing_cv(candidate: Person):
-    point = 0
-    point = point + pointing_skill(candidate.Skill)
-    point = point + pointing_email_address(candidate.CanEmail, candidate.CanFullName)
-    point = point + pointing_experience_year(candidate.Years)
-    point = point + pointing_cv_image(candidate.CanImgCandi)
-    point = point + pointing_address(candidate.AddressNow)
-    point = point + pointing_soft_skill(candidate.SoftSkill)
-    point = point + pointing_interest(candidate.Interest)
-    personal_summary = PersonalSummary(candidate.CanFullName,
-                                       candidate.CanDob,
-                                       candidate.CanAddress1,
-                                       candidate.CanEmail,
-                                       candidate.CanTelNum,
-                                       candidate.CanEthnic,
-                                       candidate.CanReligion,
-                                       candidate.CanNominee,
-                                       candidate.CanNationality,
-                                       candidate.CanIdentityName,
-                                       candidate.CanNearestWork,
-                                       candidate.Career)
-    point = point + pointing_personal_summary(personal_summary)
-    point = point + pointing_spell(candidate)
-    point = point + pointing_diploma(candidate.Diploma)
-    point = point + rank_diploma(candidate.Diploma)
-    point = point + pointing_tel_num(candidate.CanTelNum)
-    # point = point + rank_introduction()
-    return point/NUMBER_OF_CRITERIA
-
-def 
